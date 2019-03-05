b14fdebcad1a64b7634d756f67f6c0e885783c31
diego.furtado <diego.furtado@liferay.com>
2019-03-04 23:40:27 -0300

    creating other unit tests  and getting better the code

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,17 @@
 module.exports = (app) => {
-    const findAll = () => {
-        return app.db('users').select();
+    const findAll = (filter = {}) => {
+        return app.db('users').where(filter).select();
     };
 
-    const save = (user) => {
+    const save = async (user) => {
+        if (!user.name) return { error: 'Name is required!'};
+        if (!user.mail) return { error: 'Email is required!'};
+        if (!user.password) return { error: 'Password is required!'};
+        
+        const userDb = await findAll({mail: user.mail});
+        if (userDb && userDb.length > 0)
+            return {error: 'User already exists!'};
+
         return app.db('users').insert(user, '*');
     }
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@gmail.com`;
+
 test ('Should list all users', () => {
     return request(app).get('/users')
         .then((res) => {
@@ -11,7 +13,6 @@ test ('Should list all users', () => {
 })
 
 test('Should add a user with success', () => {
-    const mail = `${Date.now()}@gmail.com`;
     return request(app).post('/users')
         .send({name: 'Antonio Citizens', mail, password: 'root'})
         .then((res) => {
@@ -19,3 +20,44 @@ test('Should add a user with success', () => {
             expect(res.body.name).toBe('Antonio Citizens');
     });
 })
+
+//Three ways to work with async tests
+
+//First Strategy
+test('Should not insert a user without name', () => {
+    return request(app).post('/users')
+        .send({mail: 'anything@gmail.com', password: 'root'})
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Name is required!');
+        })
+        .catch(err => done.fail(err));
+});
+
+//Second Strategy: using awaiting
+test ('Should not insert a user withou email', async () => {
+    const result = await request(app).post('/users')
+        .send({name: 'Diego Furtado', password: 'root'});
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('Email is required!');
+});
+
+//Third Strategy
+test ('Should not insert a user without password', (done) => {
+    request(app).post('/users')
+        .send({name: 'Diego Furtado', mail: 'diegotomfurtado@gmail.com'})
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Password is required!');
+            done();
+        });
+});
+
+test ('Should not insert a user with existing name', () => {
+    return request(app).post('/users')
+        .send({name: 'Antonio Citizens', mail, password: 'root'})
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('User already exists!');
+    });
+});
